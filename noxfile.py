f8ff179b39fb76a111872f2f1ac23cad728d6aed
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2025-01-26 18:26:49 +0330

    Bump nox version Update Python test versions

--- noxfile.py
+++ noxfile.py
@@ -23,8 +23,8 @@ except ImportError:
 
 
 package = "cjolowicz_cookiecutter_hypermodern"
-python_versions = ["3.10", "3.9", "3.8", "3.7"]
-nox.needs_version = ">= 2021.6.6"
+python_versions = ["3.10", "3.11", "3.12", "3.13"]
+nox.needs_version = ">= 2024.10.9"
 nox.options.sessions = (
     "pre-commit",
     "safety",
